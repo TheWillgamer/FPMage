b7ddb95357761ba2d3aff6156f03cc9580cd3335
William Jeon <iamwilly99@gmail.com>
2022-07-29 20:42:29 -0700

    almost fixed movement

--- Assets/Scripts/MoveCamera.cs
+++ Assets/Scripts/MoveCamera.cs
@@ -3,22 +3,36 @@ using UnityEngine;
 
 public class MoveCamera : NetworkBehaviour
 {
-    [SerializeField]
-    private Transform playerHead;
+    private float xRotation;
+    private float sensitivity = 50f;
+    private float sensMultiplier = 1f;
 
     public override void OnStartClient()
     {
         base.OnStartClient();
         if (base.IsOwner)
         {
-            gameObject.SetActive(true);
+            transform.GetChild(0).gameObject.SetActive(true);
         }
     }
-    //void Update()
-    //{
-    //    if (base.IsOwner)
-    //    {
-    //        transform.position = playerHead.transform.position;
-    //    }
-    //}
+
+    private void Update()
+    {
+        if (!base.IsOwner)
+            return;
+
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.fixedDeltaTime * sensMultiplier;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.fixedDeltaTime * sensMultiplier;
+
+        //Find current look rotation
+        Vector3 rot = transform.localRotation.eulerAngles;
+        float desiredX = rot.y + mouseX;
+
+        //Rotate, and also make sure we dont over- or under-rotate.
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        //Perform the rotations
+        transform.localRotation = Quaternion.Euler(xRotation, desiredX, 0);
+    }
 }

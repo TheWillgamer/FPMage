0e437b0fa145e99a48f24ef675e9958f0cb73c28
William Jeon <iamwilly99@gmail.com>
2022-07-31 14:42:02 -0700

    lobby and worlds and fishnet pro

--- Assets/FishNet/Runtime/Editor/ScriptingDefines.cs
+++ Assets/FishNet/Runtime/Editor/ScriptingDefines.cs
@@ -33,7 +33,9 @@ namespace FishNet
 
             /* Remove pro define if not on pro. This might look a little
              * funny because the code below varies depending on if pro or not. */
-            
+            //PROSTART
+            if (1 == 2)
+                //PROEND
 #pragma warning disable CS0162 // Unreachable code detected
                 modified |= definesHs.Remove(proDefine);
 #pragma warning restore CS0162 // Unreachable code detected

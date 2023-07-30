e11f6dfe17c56b26640f7b6386a098b06b63d1fa
William Jeon <iamwilly99@gmail.com>
2023-07-29 18:22:43 -0700

    fireball

---
+++ Assets/Scripts/Abilities/Projectile.cs
@@ -0,0 +1,11 @@
+using FishNet.Managing.Timing;
+using UnityEngine;
+
+public interface Projectile
+{
+    /// <summary>
+    /// Initializes projectile with force.
+    /// </summary>
+    /// <param name="force"></param>
+    void Initialize(PreciseTick pt, Vector3 force);
+}

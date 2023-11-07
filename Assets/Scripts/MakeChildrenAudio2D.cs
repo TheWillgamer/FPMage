f47df7612c39c874b6bdf4c025b5a89862d093b1
William Jeon <iamwilly99@gmail.com>
2023-11-06 17:21:51 -0800

    ksehglsa

--- Assets/Scripts/MakeChildrenAudio2D.cs
+++ Assets/Scripts/MakeChildrenAudio2D.cs
@@ -3,12 +3,14 @@ using FishNet.Object;
 
 public class MakeChildrenAudio2D : NetworkBehaviour
 {
+    [SerializeField] private Transform parent;
+
     public override void OnStartClient()
     {
         base.OnStartClient();
         if (base.IsOwner)
         {
-            foreach (Transform child in transform)
+            foreach (Transform child in parent.transform)
             {
                 child.GetComponent<AudioSource>().spatialBlend = 0f;
             }

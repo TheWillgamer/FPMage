002aa1fd993e1135cb104e51af3aa57de89bb8f4
William Jeon <iamwilly99@gmail.com>
2023-11-15 17:05:11 -0800

    FEHslkjhf

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -97,12 +97,6 @@ public class PlayerHealth : NetworkBehaviour
             ReenableGravity();
             CancelInvoke("ReenableGravity");
         }
-        if (moving && invulnerable && (Input.GetButtonDown("Fire1") || Input.GetButtonDown("Fire2") || Input.GetButtonDown("Fire3") || Input.GetButtonDown("Fire4")))
-        {
-            invulnerable = false;
-            CancelInvul();
-            CancelInvoke("CancelInvul");
-        }
     }
 
     private void TimeManager_OnTick()

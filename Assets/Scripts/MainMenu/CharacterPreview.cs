54c6977fdb94c52a09eb5dd26cd3c62f3681fc96
William Jeon <iamwilly99@gmail.com>
2024-06-17 17:22:54 -0700

    character select menu

---
+++ Assets/Scripts/MainMenu/CharacterPreview.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class CharacterPreview : MonoBehaviour
+{
+    [SerializeField] private GameObject[] characterModels;
+    private GameObject wizardDemo;      // Shows the wizard that was chozen
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GeneratePreview();
+    }
+
+    public void SetCharacter(int type)
+    {
+        PlayerPrefs.SetInt("Character", type);
+        GeneratePreview();
+    }
+
+    private void GeneratePreview()
+    {
+        if (wizardDemo != null)
+        {
+            Destroy(wizardDemo);
+        }
+
+        int type = PlayerPrefs.GetInt("Character", 0);
+        wizardDemo = Instantiate(characterModels[type], transform.position, transform.rotation);
+    }
+}
